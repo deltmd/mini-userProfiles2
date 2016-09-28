1c8fa59f8ed0c4393344851ce75bf7e2300ac815
Joe Whitehorn <Joe Whitehorn>
2016-09-28 13:05:10 -0600

    complete server request update

--- controller.js
+++ controller.js
@@ -1,10 +1,9 @@
-angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+angular.module('userProfiles')
+.controller('MainController', function($scope, mainService) {
+  mainService.getUsers().then( function(response) {
+    $scope.users = response.data.data;
+  })
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
-
-  $scope.getUsers();
+  // $scope.getUsers();
 
 });
